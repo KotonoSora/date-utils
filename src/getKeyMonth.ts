3a0e6e22fbf59ddc8c6fb7da858dc47a7862f841
Nguyễn Duy Thắng <thangnt@vn-sis.com>
2020-07-27 17:03:41 +0700

    improve function get key month

--- src/getKeyMonth.ts
+++ src/getKeyMonth.ts
@@ -14,39 +14,50 @@ export interface IKeymonth {
 	lastDate: string;
 }
 
-export default function getKeyMonth (props: IGetKeyMonth): IKeymonth {
-	console.log('=== START ===', { props });
+export default function getKeyMonth(props: IGetKeyMonth): IKeymonth {
 	const initDate = moment.utc(props.initDate);
 	let firstDate = initDate.clone();
 	let lastDate = initDate.clone();
+	if (props.monthStartDate >= 32) {
+		firstDate.add(-1, 'month').endOf('month');
+		lastDate = firstDate.clone().add(1, 'month');
+	} else {
+		if (initDate.clone().get('date') < props.monthStartDate) {
+			firstDate.add(-1, 'month');
+		}
+		const maxDayOfMonth = firstDate
+			.clone()
+			.endOf('month')
+			.get('date');
+		if (props.monthStartDate > maxDayOfMonth) {
+			firstDate.endOf('month');
+			lastDate = firstDate.clone().add(1, 'month');
+		} else {
+			firstDate.set('date', props.monthStartDate);
+			lastDate = firstDate
+				.clone()
+				.add(1, 'month')
+				.add(-1, 'day');
+		}
+	}
 	switch (props.monthStartDateAction) {
 		case START_DATE_ACTION.NO_CHANGE:
-			if (props.monthStartDate >= 32) {
-				firstDate.add(-1, 'month').endOf('month');
-				lastDate = firstDate.clone().add(1, 'month');
-			} else {
-				if (initDate.clone().get('date') < props.monthStartDate) {
-					firstDate.add(-1, 'month');
-				}
-				const maxDayOfMonth = firstDate
-					.clone()
-					.endOf('month')
-					.get('date');
-				if (props.monthStartDate > maxDayOfMonth) {
-					firstDate.endOf('month');
-					lastDate = firstDate.clone().add(1, 'month');
-				} else {
-					firstDate.set('date', props.monthStartDate);
-					lastDate = firstDate
-						.clone()
-						.add(1, 'month')
-						.add(-1, 'day');
-				}
-			}
 			break;
 		case START_DATE_ACTION.PREVIOUS:
+			if (lastDate.clone().add(1, 'day').day() === 6) {
+				lastDate = lastDate.clone().add(-1, 'day');
+			}
+			if (lastDate.clone().add(1, 'day').day() === 0) {
+				lastDate = lastDate.clone().add(-2, 'day');
+			}
 			break;
 		case START_DATE_ACTION.NEXT_WEEK:
+			if (lastDate.clone().add(1, 'day').day() === 6) {
+				lastDate = lastDate.clone().add(2, 'day');
+			}
+			if (lastDate.clone().add(1, 'day').day() === 0) {
+				lastDate = lastDate.clone().add(1, 'day');
+			}
 			break;
 		default:
 			break;
@@ -54,7 +65,6 @@ export default function getKeyMonth (props: IGetKeyMonth): IKeymonth {
 	let keyMonth: string = firstDate.format('YYYY-MM-DD');
 	keyMonth += '-';
 	keyMonth += lastDate.format('YYYY-MM-DD');
-	console.log('=== END ===');
 	return {
 		keyMonth,
 		firstDate: firstDate.format('YYYY-MM-DD'),
